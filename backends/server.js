4381739c51bf689e77ae46899dbf5e6f9e8b4a3e
anupam575 <tripathianupam208@gmail.com>
2025-09-20 13:24:00 +0530

    Update server.js

--- backends/server.js
+++ backends/server.js
@@ -21,7 +21,7 @@ const startServer = async () => {
       api_secret: process.env.CLOUDINARY_API_SECRET,
     });
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 4000;
     const server = app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
